664c9bc59432b9bb0b7d0246ae625b0227d552c1
Keshav Dayal <115068840+keshxvdayal@users.noreply.github.com>
2024-03-09 07:24:58 +0530

    NEXT JS BLOG

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class", // or 'media' or 'class
+  theme: {
+
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+};
+export default config;
